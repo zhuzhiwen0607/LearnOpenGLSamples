7b97fad74e04ceea9af9011051e8e18e5ee87ec7
zhuzhiwen <zhuzhiwen0607@qq.com>
2022-09-09 09:37:31 +0800

    mix texture and color in fragment shader

--- Texture/texture.fs
+++ Texture/texture.fs
@@ -12,5 +12,5 @@ void main()
 {
 	// linearly interpolate between both textures (80% container, 20% awesomeface)
 	//FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
-	FragColor = texture(ourTexture, TexCoord);
+	FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0);
 }
